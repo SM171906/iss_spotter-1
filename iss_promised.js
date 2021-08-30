eaad3f7854cff56cf817e0bf31a742b3eadb87e8
Smiti <mishra.smiti06@gmail.com>
2021-08-30 09:38:10 -0400

    u[pdated the code

---
+++ iss_promised.js
@@ -0,0 +1,39 @@
+//const request = require('request-promise-native');
+
+
+/* 
+ * Makes a request to freegeoip.app using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+
+const request = require('request-promise-native');
+
+const fetchMyIP = () => {
+  return request('https://api.ipify.org/?format=json%27);
+};
+
+const fetchCoordsByIP = (body) => {
+  const ip = JSON.parse(body).ip;
+  return request(https://freegeoip.app/json/${ip});
+}
+
+const fetchISSFlyOverTimes = (body) => {
+  const { latitude, longitude } = JSON.parse(body);
+  const url = http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude};
+  return request(url);
+}
+
+const nextISSTimesForMyLocation = () => {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    })
+}
+
+module.exports = {
+  nextISSTimesForMyLocation
+};
